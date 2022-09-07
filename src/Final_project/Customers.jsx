039daee9f9711e8f0993a68461c1b867b53b1197
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-09-07 11:03:35 +0300

    uploadig

--- src/Final_project/Customers.jsx
+++ src/Final_project/Customers.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Customers() {
-  return <div className="container d-flex">Customers</div>;
+  return <div className="container d-flex text-light">Customers</div>;
 }
