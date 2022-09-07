19b38b1cdbd9acc3b787abe7dac133c0e2ee7301
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-09-07 10:57:51 +0300

    uploadig

--- src/Final_project/Purchases.jsx
+++ src/Final_project/Purchases.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Purchases() {
-  return <div className="container d-flex gap-5"></div>;
+  return <div className="container d-flex gap-5">Purchases</div>;
 }
