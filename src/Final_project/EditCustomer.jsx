9f1823d13799db7d8244d00573e8d24db021131c
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-09-14 00:06:20 +0300

    upload

--- src/Final_project/EditCustomer.jsx
+++ src/Final_project/EditCustomer.jsx
@@ -6,7 +6,7 @@ export default function EditCustomer() {
     <div className="container">
       <input type="text" placeholder="Edit Customer" className="rounded-3" />
       <br />
-      <button className="btn btn-light mx-2 my-3">pill</button>
+      <button className="btn btn-light mx-2 my-3">update</button>
       <button className="btn btn-light my-3">delete</button>
     </div>
   );
